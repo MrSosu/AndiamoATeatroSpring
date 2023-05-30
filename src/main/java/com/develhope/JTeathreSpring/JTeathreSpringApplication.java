f0abdf2d1f14a84eee2eb2901c24e1b44f6bac6a
Dario La Placa <dariolaplacauni@gmail.com>
2023-05-30 16:47:59 +0200

    Primo Commit

---
+++ src/main/java/com/develhope/JTeathreSpring/JTeathreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.develhope.JTeathreSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JTeathreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JTeathreSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/develhope/JTeathreSpring/entities/Liveshow.java
@@ -0,0 +1,29 @@
+package com.develhope.JTeathreSpring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Liveshow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false)
+    private int durationInMinutes;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private String genre;
+    @ManyToOne
+    private Hall hall;
+}
